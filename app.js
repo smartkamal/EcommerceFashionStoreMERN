5186d5d05df30c04390eda85d118b9d4d34192b8
ASUS <thumalee@outlook.com>
2020-04-05 01:07:59 +0530

    Validated user sign up details

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
+const expressValidator = require('express-validator');
 require('dotenv').config();
 
 //require routes
@@ -21,6 +22,7 @@ mongoose.connect(process.env.DATABASE,{
 app.use(morgan('dev'));
 app.use(bodyParser.json());
 app.use(cookieParser());
+app.use(expressValidator());
 
 
 //routes
--- controllers/users.js
+++ controllers/users.js
@@ -1,14 +1,17 @@
 const User = require('../models/user');
+const {errorHandler} = require("../helpers/dbErrorHandler");
 
 exports.signup = (req,res) =>{
-    console.log("req.body",req.body);
+    //console.log("req.body",req.body);
     const user = new User(req.body);
     user.save((error,user)=>{
         if (error){
             return res.status(400).json({
-                error
+                error: errorHandler(error)
             });
         }
+        user.salt = undefined;
+        user.hashed_password = undefined;
         res.json({
             user
         })
