e1501ed4b8e0244d73949ee98468478170371627
hariidasa <dmhedasanayake@gmail.com>
2020-04-05 12:31:01 +0530

    CartHandler created with adding function

---
+++ core/cartHandler.js
@@ -0,0 +1,20 @@
+export const addItem = (item,next) =>{
+
+    let cart =[];
+    if(typeof window!=='undefined'){
+        if(localStorage.getItem('cart')){
+            cart=JSON.parse(localStorage.getItem('cart'))
+        }
+        cart.push({
+            ...item,
+            count: 1
+        });
+
+        cart = Array.from(new Set(cart.map((p)=>(p.id)))).map(id=>{
+            return cart.find(p._id===id)
+        });
+
+        localStorage.setItem('cart',JSON.stringify(cart))
+        next();
+    }
+};
