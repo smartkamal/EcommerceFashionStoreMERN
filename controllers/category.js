a2ff29caceffc35e339d36a2882f731ac1db0794
ASUS <thumalee@outlook.com>
2020-04-06 22:00:21 +0530

    Implemented the methods to update, delete and list the categories

--- controllers/category.js
+++ controllers/category.js
@@ -13,3 +13,58 @@ exports.addCategory = (req,res)=>{
         res.json({content});
     });
 };
+
+exports.findCategoryById = (req, res, next, id) => {
+    Category.findById(id).exec((error,category) => {
+        if (error || !category) {
+            return res.status(400).json({
+                error: 'Category not found'
+            });
+        }
+        req.productCat = category;
+        next();
+    });
+};
+
+exports.contentRead = (req, res) => {
+    return res.json(req.productCat);
+};
+
+
+exports.updateCategory = (req, res) => {
+    const  cat = req.productCat
+    cat.categoryName = req.body.categoryName
+    cat.save((error, content) => {
+        if(error){
+            return res.status(400).json({
+                error: errorHandler(error)
+            });
+        }
+        res.json(content);
+    });
+};
+
+exports.deleteCategory = (req, res) => {
+    const  cat = req.productCat
+    cat.remove((error) => {
+        if(error){
+            return res.status(400).json({
+                error: errorHandler(error)
+            });
+        }
+        res.json({
+            message : 'Category deleted'
+        });
+    });
+};
+
+exports.listCategories = (req, res) => {
+    Category.find().exec((error, content) => {
+        if(error){
+            return res.status(400).json({
+                error: errorHandler(error)
+            });
+        }
+        res.json(content);
+    });
+};
