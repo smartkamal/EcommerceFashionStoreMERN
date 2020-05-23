148ef1b7a81ee5ae043ba79fa218b2d824c48e2c
kgeor <kgeorgesilva97@gmail.com>
2020-05-23 07:42:55 +0530

    app.js modified to catch errors and added an import

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 const expressValidator = require('express-validator');
 const cors = require('cors');
+const path = require('path');
 
 
 require('dotenv').config();
