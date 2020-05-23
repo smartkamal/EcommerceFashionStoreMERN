0f12c7d8fb4f46b21d47873668a40ca519095089
kgeor <kgeorgesilva97@gmail.com>
2020-05-23 07:26:24 +0530

    app.js modified

--- app.js
+++ app.js
@@ -53,11 +53,6 @@ const port = process.env.PORT || 8000;
 
 if(process.env.NODE_ENV){
     app.use(express.static('./frontend/build'))
-
-    const path = require('path');
-    app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-    });
 }
 
 app.listen(port, () =>{
